68293fe1459dfd20dd0bdafd98744be26d7fb292
kyberliem <cyberliem.civil@gmail.com>
2018-04-24 11:26:41 +0700

    working

--- stat/statFetcherRunner_Test_fw.go
+++ stat/statFetcherRunner_Test_fw.go
@@ -9,23 +9,23 @@ type FetcherRunnerTest struct {
 	fr FetcherRunner
 }
 
-func NewControllerRunnerTest(controllerRunner ControllerRunner) *ControllerRunnerTest {
-	return &ControllerRunnerTest{controllerRunner}
+func NewFetcherRunnerTest(fetcherRunner FetcherRunner) *FetcherRunnerTest {
+	return &NewFetcherRunnerTest{fetcherRunner}
 }
 
-func (self *ControllerRunnerTest) TestAnalyticStorageControlTicker(nanosec int64) error {
-	if err := self.cr.Start(); err != nil {
+func (self *FetcherRunnerTest) TestFetcher(nanosec int64) error {
+	if err := self.fr.Start(); err != nil {
 		return err
 	}
 	startTime := time.Now()
-	t := <-self.cr.GetAnalyticStorageControlTicker()
+	t := <-self.fr.GetAnalyticStorageControlTicker()
 	timeTook := t.Sub(startTime).Nanoseconds()
 	upperRange := nanosec + nanosec/10
 	lowerRange := nanosec - nanosec/10
 	if timeTook < lowerRange || timeTook > upperRange {
 		return fmt.Errorf("expect ticker in between %d and %d nanosec, but it came in %d instead", lowerRange, upperRange, timeTook)
 	}
-	if err := self.cr.Stop(); err != nil {
+	if err := self.fr.Stop(); err != nil {
 		return err
 	}
 	return nil
