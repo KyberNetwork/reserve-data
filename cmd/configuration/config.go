3f96deb4d503af4c89e4f9f00167ad058411e05d
NNagato <darkdragon236@gmail.com>
2018-05-09 00:50:01 +0700

    fix pricing fee setrate

--- cmd/configuration/config.go
+++ cmd/configuration/config.go
@@ -85,7 +85,9 @@ type Config struct {
 	SetRateAddress     ethereum.Address
 	ThirdPartyReserves []ethereum.Address
 
-	ApiKey    string
+	BeginBlockSetRate uint64
+	ApiKey            string
+
 	ChainType string
 }
 
@@ -149,6 +151,7 @@ func (self *Config) AddStatConfig(settingPath SettingPaths, addressConfig common
 	}
 
 	apiKey := GetAPIKey(settingPath.secretPath)
+	beginBlockSetRate := GetBeginBlockSetRate(settingPath.settingPath)
 
 	self.StatStorage = statStorage
 	self.AnalyticStorage = analyticStorage
@@ -163,6 +166,7 @@ func (self *Config) AddStatConfig(settingPath SettingPaths, addressConfig common
 	self.NetworkAddress = networkAddr
 	self.WhitelistAddress = whitelistAddr
 	self.SetRateAddress = setRateAddr
+	self.BeginBlockSetRate = beginBlockSetRate
 	self.ApiKey = apiKey
 }
 
