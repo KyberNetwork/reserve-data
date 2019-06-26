f615fffc2ce2da309046f0df86096e0687357b63
Diep Pham <me@favadi.com>
2019-06-26 14:32:12 +0700

    stop reading token from file in simulation mode

--- cmd/configuration/tokens_config.go
+++ cmd/configuration/tokens_config.go
@@ -1,8 +1,6 @@
 package configuration
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 
 	"github.com/KyberNetwork/reserve-data/common"
@@ -205,48 +203,9 @@ func mustGetTokenConfig(kyberEnv string) map[string]common.Token {
 	if avail {
 		return result
 	}
-	if kyberEnv == common.SimulationMode {
-		result, err := loadTokenFromFile(simSettingPath)
-		if err != nil {
-			log.Panicf("cannot load data from pre-defined simluation setting file, err: %v", err)
-		}
-		return result
-
-	}
 	if kyberEnv == common.ProductionMode {
 		return TokenConfigs[common.MainnetMode]
 	}
 	log.Panicf("cannot get token config for mode %s", kyberEnv)
 	return nil
-}
-
-type token struct {
-	Address  string `json:"address"`
-	Name     string `json:"name"`
-	Decimals int64  `json:"decimals"`
-	Internal bool   `json:"internal use"`
-	Active   bool   `json:"listed"`
-}
-
-type tokenData struct {
-	Tokens map[string]token `json:"tokens"`
-}
-
-func loadTokenFromFile(filePath string) (map[string]common.Token, error) {
-	var (
-		result = make(map[string]common.Token)
-		tokens tokenData
-	)
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return result, err
-	}
-	if err = json.Unmarshal(data, &tokens); err != nil {
-		return result, err
-	}
-	for id, t := range tokens.Tokens {
-		token := common.NewToken(id, t.Name, t.Address, t.Decimals, t.Active, t.Internal, common.GetTimepoint())
-		result[id] = token
-	}
-	return result, nil
 }
