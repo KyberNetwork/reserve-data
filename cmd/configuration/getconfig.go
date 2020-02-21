18c0342a96d99c051b2b0bf747a9896f362a7c6a
Hoàng Hà <halink0803@gmail.com>
2020-02-21 11:51:18 +0700

    update config

--- cmd/configuration/getconfig.go
+++ cmd/configuration/getconfig.go
@@ -81,7 +81,10 @@ func InitAppState(authEnbl bool, ac ccfg.AppConfig) *AppState {
 	if len(callClients) == 0 {
 		l.Warn("no backup client available")
 	}
-	callClients = append(callClients, mainClient)
+	callClients = append(callClients, &common.EthClient{
+		Client: mainClient,
+		URL:    ac.Node.Main,
+	})
 
 	bc := blockchain.NewBaseBlockchain(
 		client, mainClient, map[string]*blockchain.Operator{},
