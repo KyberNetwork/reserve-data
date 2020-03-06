84e2985905c414b25409721d6854a6bb347baf71
Tran Minh Duc <ductm54@gmail.com>
2020-03-06 18:16:59 +0700

    handle parsing error

--- feed-provider/common/feed.go
+++ feed-provider/common/feed.go
@@ -3,6 +3,6 @@ package common
 type Feed struct {
 	Ask   float64 `json:"ask,string"`
 	Bid   float64 `json:"bid,string"`
-	Error error
-	Valid bool
+	Error error   `json:"error,omitempty"`
+	Valid bool    `json:"valid"`
 }
--- feed-provider/httpserver/server.go
+++ feed-provider/httpserver/server.go
@@ -6,6 +6,19 @@ import (
 	"go.uber.org/zap"
 )
 
+//MiddlewareHandler handle middleware error
+func MiddlewareHandler(c *gin.Context) {
+	c.Next()
+	defer func(c *gin.Context) {
+		if len(c.Errors) > 0 {
+			c.JSON(
+				c.Writer.Status(),
+				c.Errors,
+			)
+		}
+	}(c)
+}
+
 //HTTPServer Httpserver that serve exporter
 type HTTPServer struct {
 	sugar *zap.SugaredLogger
@@ -25,9 +38,15 @@ func NewHTTPServer(sugar *zap.SugaredLogger, s storage.Storage, port string) (*H
 //Run start HTTPServer
 func (h *HTTPServer) Run() error {
 	r := gin.Default()
+	r.Use(MiddlewareHandler)
 	r.GET("/feed/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		data := h.s.Load(name)
+		if data.Error != nil {
+			c.Abort()
+			c.Error(data.Error)
+			return
+		}
 		c.JSON(200, data)
 	})
 	return r.Run(h.port)
