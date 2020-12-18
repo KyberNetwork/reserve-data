d777432b99e75bdeab441337002d28a15274cd4d
GiangTran <secmask@gmail.com>
2020-12-18 18:01:30 +0700

    add binance eth-usdc feed

--- common/usd_data.go
+++ common/usd_data.go
@@ -20,6 +20,7 @@ type USDData struct {
 	Timestamp             uint64
 	CoinbaseETHUSDDAI5000 FeedProviderResponse `json:"CoinbaseETHUSDDAI5000"`
 	CurveDAIUSDC10000     FeedProviderResponse `json:"CurveDAIUSDC10000"`
+	BinanceETHUSDC10000   FeedProviderResponse `json:"BinanceETHUSDC10000"`
 }
 
 // ToMap convert to map result.
@@ -28,5 +29,6 @@ func (u USDData) ToMap() map[string]interface{} {
 		"Timestamp":                         u.Timestamp,
 		feed.CoinbaseETHUSDDAI5000.String(): u.CoinbaseETHUSDDAI5000,
 		feed.CurveDAIUSDC10000.String():     u.CurveDAIUSDC10000,
+		feed.BinanceETHUSDC10000.String():   u.BinanceETHUSDC10000,
 	}
 }
